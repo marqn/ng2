8a62db1fccec4b38c43b3f00bc8891d3e02d1d7c
Marqn <marqnpl@gmail.com>
2019-03-15 09:17:24 +0100

    2. Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +9,14 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Marqn'
-  };
+  heroes = HEROES;
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    console.log(hero.id, hero.name);
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
